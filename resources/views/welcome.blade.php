ec9eb8dbae1ab27bb2d4e4ee1e6f372d4b69caee
rapleeee <raflimaulanaaaa@gmail.com>
2026-01-15 09:28:47 +0700

    update button cek eligible PTN dan Hasil TKA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            Cek Eligible PTN
+                            Cek Eligible PTN dan Hasil TKA
                         </a>
                 </div>
                 
